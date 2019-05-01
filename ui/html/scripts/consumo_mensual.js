625c53295992f6f7a1cfbc55b7bdf264d387c00b
Sergio Merino Alonso <sergio.merino.a@gmail.com>
2019-05-01 20:46:30 +0100

    Parametrize the getData method

--- ui/html/scripts/consumo_mensual.js
+++ ui/html/scripts/consumo_mensual.js
@@ -1,23 +1,46 @@
-var product = "Aceitunas"
-var region = "Andalucía"
-var year = "2004"
-var field = "Volumen (miles de kg)"
 
-// Prepare API query
-function getUrl() {
+var fieldsMap = { "volume": "Volumen (miles de kg)"}
+
+/**
+ * Request a product data
+ * @param product The product to query
+ * @param region The region to query
+ * @param year The year to query
+ * @param field the desired field to query
+ * @param callback method to execute with the data
+ */
+function requestData(product, region, year, field, callback) {
+  var url = getUrl(product, region, year, field)
+  d3.json(url).then(callback)
+}
+
+/**
+ * Construct query Http call
+ * @param product The product to query
+ * @param region The region to query
+ * @param year The year to query
+ * @param field the desired field to query
+ * @return the Http get Url with the query
+ */
+function getUrl(product, region, year, field) {
     var baseUrl = 'http://localhost:5000/test2'
     var filter = '{"Producto":"' + product + '","Región":"' + region + '","Año":"' + year + '"}'
     var projection = '{"' + field + '":1,"Mes":1,"Producto":0,"Categoría":0}'
     var dataUrl = encodeURI(baseUrl + '?max_results=300&where=' + filter + '&projection=' + projection);
     return dataUrl
 }
-// Do API query to get the data 
-d3.json(getUrl()).then(showData)
+
+var product = "Aceitunas"
+var region = "Andalucía"
+var year = "2004"
+var fieldToQuery = "volume"
+var field = fieldsMap[fieldToQuery]
+requestData(product, region, year, field, showData)
 
 // Print graphs
 function showData(data) {
     // Define the title
-    d3.select('body').append('div').text(field + " de " + product + " en " + region + " en " + year)
+    d3.select('body').append('div').text(fieldsMap[field] + " de " + product + " en " + region + " en " + year)
 	
     // Get only relevant data
     var monthsData = data._items
