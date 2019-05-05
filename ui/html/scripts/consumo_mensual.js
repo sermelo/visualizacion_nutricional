502d9a338d1d81e2df15eff66bc9736b20154fcb
Sergio Merino Alonso <sergio.merino.a@gmail.com>
2019-05-05 09:47:29 +0100

    Create queries file as an interface for data requests

--- ui/html/scripts/consumo_mensual.js
+++ ui/html/scripts/consumo_mensual.js
@@ -44,19 +44,6 @@ function updateGraph(dataProduct, dataRegion, dataYear, fieldSortName) {
 }
 
 /**
- * Request a product data
- * @param product The product to query
- * @param region The region to query
- * @param year The year to query
- * @param field the desired field to query
- * @param callback method to execute with the data
- */
-function requestData(product, region, year, field, callback) {
-    var url = getUrl(product, region, year, field)
-    d3.json(url).then(callback)
-}
-
-/**
  * Create empty graph
  */
 function createEmptyGraph() {
@@ -74,22 +61,6 @@ function createEmptyGraph() {
     yAxisContainer = container
         .append("g")
         .attr("transform", "translate(" + (margin.left) + ", 0)")
-}
-
-/**
- * Construct query Http call
- * @param product The product to query
- * @param region The region to query
- * @param year The year to query
- * @param field the desired field to query
- * @return the Http get Url with the query
- */
-function getUrl(product, region, year, field) {
-    var baseUrl = 'http://localhost:5000/test3'
-    var filter = '{"Producto":"' + product + '","Región":"' + region + '","Año":' + year + '}'
-    var projection = '{"' + field + '":1,"Mes":1,"Producto":0,"Categoría":0}'
-    var dataUrl = encodeURI(baseUrl + '?max_results=300&where=' + filter + '&projection=' + projection);
-    return dataUrl
 }
 
 /**
