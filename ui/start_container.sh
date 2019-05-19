6a259cf7009645f880ca1fe467829bc59f5e0fbe
Sergio Merino Alonso <sergio.merino.a@gmail.com>
2019-05-19 17:54:58 +0100

    Mound absolute path volume to nginx container

--- ui/start_container.sh
+++ ui/start_container.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 html_path="`dirname \"$0\"`/html/"
-echo $html_path
+abs_html_path=`realpath ${html_path}`
 
-docker run -d --rm -v $html_path:/usr/share/nginx/html:ro -p 8000:80 --name graphics_nginx nginx
+echo "${abs_html_path}"
+
+docker run -d --rm -v ${abs_html_path}:/usr/share/nginx/html:ro -p 8000:80 --name graphics_nginx nginx
 echo "http://localhost:8000"
 
