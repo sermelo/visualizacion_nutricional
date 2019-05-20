625db87c6b694316e35c76b3f08c0e9dd1a58d80
Sergio Merino Alonso <sergio.merino.a@gmail.com>
2019-05-20 22:10:50 +0100

    Show date comparative graphs

---
+++ ui/html/scripts/date_base.js
@@ -0,0 +1,61 @@
+var model = {
+    "primaryKey": "Año",
+    "secondaryKey1": "Producto",
+    "secondaryKey2": "Región",
+}
+
+function printDates(data) {
+    var data = data._items
+    var options = d3.select("#Año").append("select").attr("multiple", "multiple").on('change',changeOption)
+    options
+        .selectAll("option")
+        .data(data)
+        .enter()
+        .append("option")
+        .attr("value", function(d){
+            return d._id
+        })
+        .text(function(d){
+            return d._id
+        })
+        .on("dblclick", function(d){
+            updateGraph(this.value)
+        })
+}
+
+function printProducts(data) {
+    var data = data._items
+    options = d3.select("#Producto").append("select").on('change',changeOption)
+    options
+        .selectAll("option")
+        .data(data)
+        .enter()
+        .append("option")
+        .attr("value", function(d){
+            return d._id
+        })
+        .text(function(d){
+            return d._id
+        })
+}
+
+function printRegions(data) {
+    data = data._items
+    options = d3.select("#Región").append("select").on('change',changeOption)
+    options
+        .selectAll("option")
+        .data(data)
+        .enter()
+        .append("option")
+        .attr("value", function(d){
+            return d._id
+        })
+        .text(function(d){
+            return d._id
+        })
+}
+
+getUniqProducts(printProducts)
+getUniqRegions(printRegions)
+getUniqYears(printDates)
+
