2928af71d93082ed90a71ecd780e623e1ecf1055
Sergio Merino Alonso <sergio.merino.a@gmail.com>
2019-06-02 22:20:52 +0100

    When requesting data, request fields to show

--- ui/html/scripts/db_queries.js
+++ ui/html/scripts/db_queries.js
@@ -6,10 +6,9 @@ var baseUrl = 'http://localhost:5000/'
  * @param primaryKey The primaryKey value
  * @param secondaryKey1 The secondaryKey1 value
  * @param secondaryKey2 The secondaryKey2 value
- * @param field the desired field to query
  * @param callback method to execute with the data
  */
-function requestProductData(primaryKey, secondaryKey1, secondaryKey2, field, callback) {
+function requestProductData(primaryKey, secondaryKey1, secondaryKey2, callback) {
     console.log("Requesting: " + primaryKey + " " + secondaryKey1 + " " + secondaryKey2)
     var url = getUrl(primaryKey, secondaryKey1, secondaryKey2, field)
     d3.json(url).then(callback)
@@ -82,7 +81,7 @@ function getUrl(primaryKey, secondaryKey1, secondaryKey2, field) {
     })
     filter = filter.substring(0, filter.length-1);
     filter += "}"
-    var projection = '{"Producto":1,"' + field + '":1,"Año":1,"Mes":1,"Región":1,"Categoría":0}'
+    var projection = '{"Producto":1,"Precio medio kg":1,"Consumo per capita":1,"Gasto per capita":1,"Penetración (%)":1,"Año":1,"Mes":1,"Región":1,"Categoría":0}'
     var sort = '[("Mes",1)]'
     var dataUrl = encodeURI(endpoint + "?max_results=300&where=" + filter + "&projection=" + projection + "&sort=" + sort)
     return dataUrl
